4d57ea5705ca107bef0167ecec8d6f6d7e52114c
Fabrício <fabriciolak12@gmail.com>
2023-02-06 11:49:51 -0300

    feat: add context provider component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { App } from '@/App'
 
 import { globalStyles } from '@/styles/global'
+import { ActivityContextProvider } from './contexts/ActivityContext'
 
 globalStyles()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ActivityContextProvider>
+      <App />
+    </ActivityContextProvider>
   </React.StrictMode>,
 )
