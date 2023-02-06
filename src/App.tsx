4d737150d1fbbc62d3662dbfe3ed7ab88b95af6f
Fabrício <fabriciolak12@gmail.com>
2023-02-06 11:52:11 -0300

    feat: reformulated the way of passing the props and created a div to center the content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { Activity } from "@/components/Activity";
+import { CreateActivity } from "@/components/CreateActivity";
+import { useActivity } from "./hooks/useActivity";
 
 export function App() {
+  const { activities } = useActivity()
+
   return (
-    <Activity />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh' }}>
+      <CreateActivity />
+      {activities && (
+        activities.map((activity, index) => (
+          <Activity
+            title={activity.title}
+            startedDate={activity.startedDate}
+            startedTime={activity.startedTime}
+            status={activity.status}
+            key={activity.id}
+          />
+        ))
+      )}
+    </div>
   )
 }
