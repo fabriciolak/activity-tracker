41bf664303480ecdff91b4210d43090087924808
Fabrício <fabriciolak12@gmail.com>
2023-02-27 11:44:11 -0300

    chore: added some things

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Activity } from "./components/Activity";
+import { CreateActivity } from "./components/CreateActivity";
 
 export function App() {
   return (
     <div>
-      <Activity />
+      <Activity title='Title' uptime='00:00' date='Segunda, Fev 3' startEndDate='data' />
+
+      <CreateActivity />
     </div>
   )
 }
